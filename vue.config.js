0ba39ad2c4f25d1acf8924bc0171739d90563d88
liaokui <13047903316lelk@gmail.com>
2020-03-24 23:31:39 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // publicPath: process.env.NODE_ENV === 'production' ? './public/admin/dist/' : '/',
-  publicPath: '/',
+  publicPath: './',
   assetsDir: 'static',
   lintOnSave: true,
   productionSourceMap: false,
