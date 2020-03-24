b994407082858ec0689c1022de86e7b982202a84
liaokui <13047903316lelk@gmail.com>
2020-03-24 23:10:49 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './public/admin/dist/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? './public/admin/dist/' : '/',
+  publicPath: './',
   assetsDir: 'static',
   lintOnSave: true,
   productionSourceMap: false,
